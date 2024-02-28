8ef76ce78132a02378af4236358bef6064aab461
baked <tristancilltseinleben@gmail.com>
2024-02-28 21:42:37 +0100

    app router

--- pages/api/[[...catchAll]].ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-
-export const config = {
-  runtime: "edge",
-};
-
-export default function CatchAllRoute(req: NextRequest) {
-  return new Response(req.nextUrl.href);
-}
